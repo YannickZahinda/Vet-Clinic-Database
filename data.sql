685e8f21f2c061e86e08bd025061feed325967fd
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-06-07 14:55:57 +0200

    added data to database

--- data.sql
+++ data.sql
@@ -1,5 +1,6 @@
 /* Populate database with sample data. */
 
-INSERT INTO animals (name) VALUES ('Luna');
-INSERT INTO animals (name) VALUES ('Daisy');
-INSERT INTO animals (name) VALUES ('Charlie');
+INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('Agumon','2020/02/3',0,TRUE,20.23);
+INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('Gabumon','2018/11/15',1,FALSE,8);
+INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('Pikachu','2021/07/1',2,FALSE,15.04);
+INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('Devimon','2017/05/12',5,TRUE,11.0);
