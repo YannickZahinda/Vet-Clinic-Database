df2c019884b1b5e5a5059b46fe6410c1b05aa8a4
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-06-09 15:26:14 +0200

    add some column data

--- data.sql
+++ data.sql
@@ -9,3 +9,6 @@ INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('S
 INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('Angemon','2005-06-12',1,TRUE,-45);
 INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('Boarmon','2005-06-07',7,TRUE,20.4);
 INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('Ditto','2022-05-14',4,TRUE,22);
+INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('Charmander','2020-02-08',0,TRUE,-11);
+INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('Plantmon','2021-11-15',2,TRUE,-5.4);
+INSERT INTO animals(name,date_of_birth,escape_attempt,neutered,weight) VALUES('Blossom','1998-10-13',3,TRUE,17);
