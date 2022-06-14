c44bf9e7114e1dc2285a5b8e5e075bb7a7f4cb33
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-06-14 14:30:56 +0200

    insert new data into owners

--- data.sql
+++ data.sql
@@ -120,3 +120,6 @@ VALUES ('2020-05-24', (SELECT A.id FROM animals A WHERE A.name = 'Agumon'),
 
 
   INSERT INTO visits(animal_id, vet_id, date_of_visit) SELECT * FROM (SELECT id FROM animals) animals_ids, (SELECT id FROM vets) vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours') visit_timestamp;
+
+  insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+
