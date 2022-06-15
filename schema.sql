b5e8389c9e33f5efcbff5caee507c3756a7605bb
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-06-15 13:12:57 +0200

    create index on visits

--- schema.sql
+++ schema.sql
@@ -31,4 +31,8 @@ CREATE TABLE visits (id INT GENERATED ALWAYS AS IDENTITY, date_of_visit DATE, an
 
 ALTER TABLE owners ADD COLUMN email varchar(250);
 
+
+
 CREATE INDEX full_name_asc ON owners(full_name ASC);
+
+CREATE INDEX vet_id_asc ON visits(vet_id ASC);
